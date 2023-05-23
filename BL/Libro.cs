9e8949877a71b599a6a1585adb5b5ab87b3feefe
JavierFloresMartinez <javierfm267@gmail.com>
2023-05-23 17:10:22 -0600

    Servicios

--- BL/Libro.cs
+++ BL/Libro.cs
@@ -129,6 +129,55 @@ namespace BL
             return result;
         }
 
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.BusquedaLibrosContext contex = new DL.BusquedaLibrosContext())
+                {
+                    var RowsAfected = contex.Libros.FromSqlRaw("GetAll").ToList();
+
+                    result.Objects = new List<object>();
+
+                    if (contex != null)
+                    {
+                        foreach (var obj in RowsAfected)
+                        {
+                            ML.Libro libro = new ML.Libro();
+                            libro.IdLibro = (int)obj.IdLibro;
+                            libro.Autor = new ML.Autor();
+                            libro.Autor.IdAutor = (int)obj.IdAutor;
+                            libro.Autor.Nombre = obj.IdAutorNavigation.Nombre;
+                            libro.Autor.ApellidoPaterno = obj.IdAutorNavigation.ApellidoPaterno;
+                            libro.Autor.ApellidoMaterno = obj.IdAutorNavigation.ApellidoMaterno;
+                            libro.Titulo = obj.TituloLibro;
+                            libro.AñoPublicacion = obj.AñoPublicacion.ToString("dd-MM-yyyy");
+                            libro.Editorial = new ML.Editorial();
+                            libro.Editorial.IdEditorial = obj.IdEditorialNavigation.IdEditorial;
+                            libro.Editorial.Nombre = obj.IdEditorialNavigation.Nombre;
+                            libro.Portada = obj.Portada;
+                            libro.Sinopsis = obj.Sinopsis;
+
+                            result.Objects.Add(libro);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "No se encontraron registros.";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
         public static ML.Result GetByAutor(ML.Libro libro)
         {
             ML.Result result = new ML.Result();
@@ -136,34 +185,37 @@ namespace BL
             {
                 using (DL.BusquedaLibrosContext contex = new DL.BusquedaLibrosContext())
                 {
-                    var RowsAfected = contex.Libros.FromSqlRaw($"LibroGetByAutor  {libro.Autor.IdAutor}").AsEnumerable().FirstOrDefault();
+                    var RowsAfected = contex.Libros.FromSqlRaw($"LibroGetByAutor  {libro.Autor.IdAutor}").ToList();
 
-                    result.Object = new object();
-                    if (RowsAfected != null)
+                    result.Objects = new List<object>();
+
+                    if (contex != null)
                     {
+                        foreach (var obj in RowsAfected)
+                        {
+                           // ML.Libro libro = new ML.Libro();
+                            libro.IdLibro = (int)obj.IdLibro;
+                            libro.Autor = new ML.Autor();
+                            libro.Autor.IdAutor = (int)obj.IdAutor;
+                            libro.Autor.Nombre = obj.IdAutorNavigation.Nombre;
+                            libro.Autor.ApellidoPaterno = obj.IdAutorNavigation.ApellidoPaterno;
+                            libro.Autor.ApellidoMaterno = obj.IdAutorNavigation.ApellidoMaterno;
+                            libro.Titulo = obj.TituloLibro;
+                            libro.AñoPublicacion = obj.AñoPublicacion.ToString("dd-MM-yyyy");
+                            libro.Editorial = new ML.Editorial();
+                            libro.Editorial.IdEditorial = obj.IdEditorialNavigation.IdEditorial;
+                            libro.Editorial.Nombre = obj.IdEditorialNavigation.Nombre;
+                            libro.Portada = obj.Portada;
+                            libro.Sinopsis = obj.Sinopsis;
 
-                        libro.IdLibro = (int)RowsAfected.IdLibro;
-                        libro.Autor = new ML.Autor();
-                        libro.Autor.IdAutor = (int)RowsAfected.IdAutor;
-                        libro.Autor.Nombre = RowsAfected.IdAutorNavigation.Nombre;
-                        libro.Autor.ApellidoPaterno = RowsAfected.IdAutorNavigation.ApellidoPaterno;
-                        libro.Autor.ApellidoMaterno = RowsAfected.IdAutorNavigation.ApellidoMaterno;
-                        libro.Titulo = RowsAfected.TituloLibro;
-                        libro.AñoPublicacion = RowsAfected.AñoPublicacion.ToString("dd-MM-yyyy");
-                        libro.Editorial = new ML.Editorial();
-                        libro.Editorial.IdEditorial = RowsAfected.IdEditorialNavigation.IdEditorial;
-                        libro.Editorial.Nombre = RowsAfected.IdEditorialNavigation.Nombre;
-                        libro.Portada = RowsAfected.Portada;
-                        libro.Sinopsis = RowsAfected.Sinopsis;
-
-                        result.Object = libro;
-
+                            result.Objects.Add(libro);
+                        }
                         result.Correct = true;
                     }
                     else
                     {
                         result.Correct = false;
-                        result.ErrorMessage = "Ocurrió un error al obtener los registros en la tabla Libros";
+                        result.ErrorMessage = "No se encontraron registros.";
                     }
                 }
 
@@ -183,34 +235,36 @@ namespace BL
             {
                 using (DL.BusquedaLibrosContext contex = new DL.BusquedaLibrosContext())
                 {
-                    var RowsAfected = contex.Libros.FromSqlRaw($"LibroGetByTituloLibro  '{libro.Titulo}'").AsEnumerable().FirstOrDefault();
+                    var RowsAfected = contex.Libros.FromSqlRaw($"LibroGetByTituloLibro  '{libro.Titulo}'").ToList();
+                    result.Objects = new List<object>();
 
-                    result.Object = new object();
-                    if (RowsAfected != null)
+                    if (contex != null)
                     {
+                        foreach (var obj in RowsAfected)
+                        {
+                            // ML.Libro libro = new ML.Libro();
+                            libro.IdLibro = (int)obj.IdLibro;
+                            libro.Autor = new ML.Autor();
+                            libro.Autor.IdAutor = (int)obj.IdAutor;
+                            libro.Autor.Nombre = obj.IdAutorNavigation.Nombre;
+                            libro.Autor.ApellidoPaterno = obj.IdAutorNavigation.ApellidoPaterno;
+                            libro.Autor.ApellidoMaterno = obj.IdAutorNavigation.ApellidoMaterno;
+                            libro.Titulo = obj.TituloLibro;
+                            libro.AñoPublicacion = obj.AñoPublicacion.ToString("dd-MM-yyyy");
+                            libro.Editorial = new ML.Editorial();
+                            libro.Editorial.IdEditorial = obj.IdEditorialNavigation.IdEditorial;
+                            libro.Editorial.Nombre = obj.IdEditorialNavigation.Nombre;
+                            libro.Portada = obj.Portada;
+                            libro.Sinopsis = obj.Sinopsis;
 
-                        libro.IdLibro = (int)RowsAfected.IdLibro;
-                        libro.Autor = new ML.Autor();
-                        libro.Autor.IdAutor = (int)RowsAfected.IdAutor;
-                        libro.Autor.Nombre = RowsAfected.IdAutorNavigation.Nombre;
-                        libro.Autor.ApellidoPaterno = RowsAfected.IdAutorNavigation.ApellidoPaterno;
-                        libro.Autor.ApellidoMaterno = RowsAfected.IdAutorNavigation.ApellidoMaterno;
-                        libro.Titulo = RowsAfected.TituloLibro;
-                        libro.AñoPublicacion = RowsAfected.AñoPublicacion.ToString("dd-MM-yyyy");
-                        libro.Editorial = new ML.Editorial();
-                        libro.Editorial.IdEditorial = RowsAfected.IdEditorialNavigation.IdEditorial;
-                        libro.Editorial.Nombre = RowsAfected.IdEditorialNavigation.Nombre;
-                        libro.Portada = RowsAfected.Portada;
-                        libro.Sinopsis = RowsAfected.Sinopsis;
-
-                        result.Object = libro;
-
+                            result.Objects.Add(libro);
+                        }
                         result.Correct = true;
                     }
                     else
                     {
                         result.Correct = false;
-                        result.ErrorMessage = "Ocurrió un error al obtener los registros en la tabla Libros";
+                        result.ErrorMessage = "No se encontraron registros.";
                     }
                 }
 
@@ -230,34 +284,37 @@ namespace BL
             {
                 using (DL.BusquedaLibrosContext contex = new DL.BusquedaLibrosContext())
                 {
-                    var RowsAfected = contex.Libros.FromSqlRaw($"LibroGetByAutorAndFechaPublicacion  {libro.Autor.IdAutor}, '{libro.AñoPublicacion}'").AsEnumerable().FirstOrDefault();
+                    var RowsAfected = contex.Libros.FromSqlRaw($"LibroGetByAutorAndFechaPublicacion  {libro.Autor.IdAutor}, '{libro.AñoPublicacion}'").ToList();
 
-                    result.Object = new object();
-                    if (RowsAfected != null)
+                    result.Objects = new List<object>();
+
+                    if (contex != null)
                     {
+                        foreach (var obj in RowsAfected)
+                        {
+                            // ML.Libro libro = new ML.Libro();
+                            libro.IdLibro = (int)obj.IdLibro;
+                            libro.Autor = new ML.Autor();
+                            libro.Autor.IdAutor = (int)obj.IdAutor;
+                            libro.Autor.Nombre = obj.IdAutorNavigation.Nombre;
+                            libro.Autor.ApellidoPaterno = obj.IdAutorNavigation.ApellidoPaterno;
+                            libro.Autor.ApellidoMaterno = obj.IdAutorNavigation.ApellidoMaterno;
+                            libro.Titulo = obj.TituloLibro;
+                            libro.AñoPublicacion = obj.AñoPublicacion.ToString("dd-MM-yyyy");
+                            libro.Editorial = new ML.Editorial();
+                            libro.Editorial.IdEditorial = obj.IdEditorialNavigation.IdEditorial;
+                            libro.Editorial.Nombre = obj.IdEditorialNavigation.Nombre;
+                            libro.Portada = obj.Portada;
+                            libro.Sinopsis = obj.Sinopsis;
 
-                        libro.IdLibro = (int)RowsAfected.IdLibro;
-                        libro.Autor = new ML.Autor();
-                        libro.Autor.IdAutor = (int)RowsAfected.IdAutor;
-                        libro.Autor.Nombre = RowsAfected.IdAutorNavigation.Nombre;
-                        libro.Autor.ApellidoPaterno = RowsAfected.IdAutorNavigation.ApellidoPaterno;
-                        libro.Autor.ApellidoMaterno = RowsAfected.IdAutorNavigation.ApellidoMaterno;
-                        libro.Titulo = RowsAfected.TituloLibro;
-                        libro.AñoPublicacion = RowsAfected.AñoPublicacion.ToString("dd-MM-yyyy");
-                        libro.Editorial = new ML.Editorial();
-                        libro.Editorial.IdEditorial = RowsAfected.IdEditorialNavigation.IdEditorial;
-                        libro.Editorial.Nombre = RowsAfected.IdEditorialNavigation.Nombre;
-                        libro.Portada = RowsAfected.Portada;
-                        libro.Sinopsis = RowsAfected.Sinopsis;
-
-                        result.Object = libro;
-
+                            result.Objects.Add(libro);
+                        }
                         result.Correct = true;
                     }
                     else
                     {
                         result.Correct = false;
-                        result.ErrorMessage = "Ocurrió un error al obtener los registros en la tabla Libros";
+                        result.ErrorMessage = "No se encontraron registros.";
                     }
                 }
 
@@ -277,34 +334,37 @@ namespace BL
             {
                 using (DL.BusquedaLibrosContext contex = new DL.BusquedaLibrosContext())
                 {
-                    var RowsAfected = contex.Libros.FromSqlRaw($"LibroGetByFechaPublicacion  '{libro.AñoPublicacion}'").AsEnumerable().FirstOrDefault();
+                    var RowsAfected = contex.Libros.FromSqlRaw($"LibroGetByFechaPublicacion  '{libro.AñoPublicacion}'").ToList();
 
-                    result.Object = new object();
-                    if (RowsAfected != null)
+                    result.Objects = new List<object>();
+
+                    if (contex != null)
                     {
+                        foreach (var obj in RowsAfected)
+                        {
+                            // ML.Libro libro = new ML.Libro();
+                            libro.IdLibro = (int)obj.IdLibro;
+                            libro.Autor = new ML.Autor();
+                            libro.Autor.IdAutor = (int)obj.IdAutor;
+                            libro.Autor.Nombre = obj.IdAutorNavigation.Nombre;
+                            libro.Autor.ApellidoPaterno = obj.IdAutorNavigation.ApellidoPaterno;
+                            libro.Autor.ApellidoMaterno = obj.IdAutorNavigation.ApellidoMaterno;
+                            libro.Titulo = obj.TituloLibro;
+                            libro.AñoPublicacion = obj.AñoPublicacion.ToString("dd-MM-yyyy");
+                            libro.Editorial = new ML.Editorial();
+                            libro.Editorial.IdEditorial = obj.IdEditorialNavigation.IdEditorial;
+                            libro.Editorial.Nombre = obj.IdEditorialNavigation.Nombre;
+                            libro.Portada = obj.Portada;
+                            libro.Sinopsis = obj.Sinopsis;
 
-                        libro.IdLibro = (int)RowsAfected.IdLibro;
-                        libro.Autor = new ML.Autor();
-                        libro.Autor.IdAutor = (int)RowsAfected.IdAutor;
-                        libro.Autor.Nombre = RowsAfected.IdAutorNavigation.Nombre;
-                        libro.Autor.ApellidoPaterno = RowsAfected.IdAutorNavigation.ApellidoPaterno;
-                        libro.Autor.ApellidoMaterno = RowsAfected.IdAutorNavigation.ApellidoMaterno;
-                        libro.Titulo = RowsAfected.TituloLibro;
-                        libro.AñoPublicacion = RowsAfected.AñoPublicacion.ToString("dd-MM-yyyy");
-                        libro.Editorial = new ML.Editorial();
-                        libro.Editorial.IdEditorial = RowsAfected.IdEditorialNavigation.IdEditorial;
-                        libro.Editorial.Nombre = RowsAfected.IdEditorialNavigation.Nombre;
-                        libro.Portada = RowsAfected.Portada;
-                        libro.Sinopsis = RowsAfected.Sinopsis;
-
-                        result.Object = libro;
-
+                            result.Objects.Add(libro);
+                        }
                         result.Correct = true;
                     }
                     else
                     {
                         result.Correct = false;
-                        result.ErrorMessage = "Ocurrió un error al obtener los registros en la tabla Libros";
+                        result.ErrorMessage = "No se encontraron registros.";
                     }
                 }
 
---
+++ SL_Kranon/Controllers/LibroController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SL_Kranon.Controllers
+{
+    public class LibroController : Controller
+    {
+        [HttpPost]
+        [Route("api/Libro/Add")]
+        public IActionResult Add([FromBody] ML.Libro libro)
+        {
+            ML.Result result = BL.Libro.Add(libro);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound(result.ErrorMessage);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("api/Libro/GetAll")]
+        public IActionResult GetAll()
+        {
+            ML.Libro libro = new ML.Libro();
+            libro.Autor = new ML.Autor();
+            ML.Result result = BL.Libro.GetAll();
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound(result.ErrorMessage);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/Libro/delete")]
+        public IActionResult delete([FromBody] int IdLibro)
+        {
+            ML.Libro libro = new ML.Libro();
+            libro.IdLibro = IdLibro;
+            ML.Result result = BL.Libro.DeleteForAutor(libro);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound(result.ErrorMessage);
+            }
+        }
+    }
+}
