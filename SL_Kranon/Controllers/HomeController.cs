cf331a20795ca2c0509813c54b3cddac8d58abf9
VHernandez0 <v.hernandez.r00@gmail.com>
2023-05-24 13:52:59 -0600

    Complementacion de servicios

--- SL_Kranon/Controllers/HomeController.cs
+++ SL_Kranon/Controllers/HomeController.cs
@@ -6,21 +6,21 @@ namespace SL_Kranon.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpPost]
-        [Route("api/Libro/Add")]
-        public IActionResult Add([FromBody] ML.Libro libro)
-        {
-            ML.Result result = BL.Libro.Add(libro);
-            if (result.Correct)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return NotFound(result.ErrorMessage);
-            }
+        //[HttpPost]
+        //[Route("api/Libro/Add")]
+        //public IActionResult Add([FromBody] ML.Libro libro)
+        //{
+        //    ML.Result result = BL.Libro.Add(libro);
+        //    if (result.Correct)
+        //    {
+        //        return Ok(result);
+        //    }
+        //    else
+        //    {
+        //        return NotFound(result.ErrorMessage);
+        //    }
 
-        }
+        //}
 
         [HttpGet]
         [Route("api/Libro/Update")]
@@ -39,11 +39,13 @@ namespace SL_Kranon.Controllers
             }
         }
 
-        [HttpPost]
-        [Route("api/Libro/GetByAutor")]
-        public IActionResult GetByAutor([FromBody] ML.Libro libro)
+        [HttpGet]
+        [Route("api/Libro/GetByAutor {IdAutor}")]
+        public IActionResult GetByAutor( int IdAutor)
         {
-
+            ML.Libro libro = new ML.Libro();
+            libro.Autor = new ML.Autor();
+            libro.Autor.IdAutor = IdAutor;
             ML.Result result = BL.Libro.GetByAutor(libro);
             if (result.Correct)
             {
@@ -54,11 +56,14 @@ namespace SL_Kranon.Controllers
                 return NotFound(result.ErrorMessage);
             }
         }
-        [HttpPost]
-        [Route("api/Libro/GetByAutorAndFechaPublicacion")]
-        public IActionResult GetByAutorAndFechaPublicacion([FromBody] ML.Libro libro)
+        [HttpGet]
+        [Route("api/Libro/GetByAutorAndFechaPublicacion {IdAutor}, {Fecha}")]
+        public IActionResult GetByAutorAndFechaPublicacion(int IdAutor, string Fecha)
         {
-
+            ML.Libro libro = new ML.Libro();
+            libro.Autor = new ML.Autor();
+            libro.Autor.IdAutor = IdAutor;
+            libro.AñoPublicacion = Fecha;
             ML.Result result = BL.Libro.GetByAutorAndFechaPublicacion(libro);
             if (result.Correct)
             {
@@ -69,10 +74,12 @@ namespace SL_Kranon.Controllers
                 return NotFound(result.ErrorMessage);
             }
         }
-        [HttpPost]
-        [Route("api/Libro/GetByFechaPublicacion")]
-        public IActionResult GetByFechaPublicacion([FromBody] ML.Libro libro)
+        [HttpGet]
+        [Route("api/Libro/GetByFechaPublicacion {Fecha}")]
+        public IActionResult GetByFechaPublicacion(string Fecha)
         {
+            ML.Libro libro = new ML.Libro();
+            libro.AñoPublicacion = Fecha;
             ML.Result result = BL.Libro.GetByFechaPublicacion(libro);
             if (result.Correct)
             {
@@ -84,6 +91,42 @@ namespace SL_Kranon.Controllers
             }
         }
 
+
+        [HttpPost]
+        [Route("api/Libro/DeleteforEditorial {IdEditorial}")]
+        public IActionResult DeleteforEditorial(int IdEditorial)
+        {
+            ML.Libro libro = new ML.Libro();
+            libro.Editorial = new ML.Editorial();
+            libro.Editorial.IdEditorial = IdEditorial;
+            ML.Result result = BL.Libro.DeleteForEditorial(libro);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound(result.ErrorMessage);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/Libro/DeleteforAutor {IdAutor}")]
+        public IActionResult DeleteforAutor(int IdAutor)
+        {
+            ML.Libro libro = new ML.Libro();
+            libro.Autor = new ML.Autor();
+            libro.Autor.IdAutor = IdAutor;
+            ML.Result result = BL.Libro.DeleteForAutor(libro);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound(result.ErrorMessage);
+            }
+        }
      
     }
 }
