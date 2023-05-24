8eec2051b3b43683226312b9fd7559a8267e190a
JavierFloresMartinez <javierfm267@gmail.com>
2023-05-24 16:53:33 -0600

    busqueda abierta funcional

--- PL_Kranon/Controllers/LibroController.cs
+++ PL_Kranon/Controllers/LibroController.cs
@@ -104,38 +104,102 @@ namespace PL_Kranon.Controllers
             return View(libro);
         }
 
-        //[HttpGet]
-        //public IActionResult GetAll(ML.Libro libro)
-        //{
+        [HttpPost]
+        public IActionResult GetAll(ML.Libro libro)
+        {
             
-        //    using (var client = new HttpClient())
-        //    {
-        //        client.BaseAddress = new Uri("http://localhost:5201/Api/");
+            ML.Autor autor = new ML.Autor();
+            ML.Editorial editorial = new ML.Editorial();
+            libro.Libros = new List<object>();
+            autor.Autores = new List<object>();
+            editorial.Editoriales = new List<object>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5235/api/");
 
-        //        var responseTask = client.PostAsJsonAsync("Usuario/GetAll", libro);
-        //        responseTask.Wait();
+                var responseTask = client.GetAsync("Autor/GetAll");
+                responseTask.Wait();
 
-        //        var resultUsuario = responseTask.Result;
+                var result = responseTask.Result;
 
-        //        if (resultUsuario.IsSuccessStatusCode)
-        //        {
-        //            var readTask = resultUsuario.Content.ReadAsAsync<ML.Result>();
-        //            readTask.Wait();
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<ML.Result>();
+                    readTask.Wait();
 
-        //            foreach (var resultItem in readTask.Result.Objects)
-        //            {
-        //                ML.Libro resultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Libro>(resultItem.ToString());
-        //                libro.Libros.Add(resultItemList);
-        //            }
-        //        }
-        //        else
-        //        {
-        //            ViewBag.Message = "Ocurrio un error al hacer la consulta";
-        //            return View("Modal");
-        //        }
+                    foreach (var resultItem in readTask.Result.Objects)
+                    {
 
-        //    }
-        //    return View(libro);
-        //}
+                        ML.Autor resultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Autor>(resultItem.ToString());
+                        autor.Autores.Add(resultItemList);
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Ocurrio un error al hacer la consulta";
+                    return View("Modal");
+                }
+
+            }
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5235/api/");
+
+                var responseTask = client.GetAsync("Editorial/GetAll");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<ML.Result>();
+                    readTask.Wait();
+
+                    foreach (var resultItem in readTask.Result.Objects)
+                    {
+
+                        ML.Editorial resultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Editorial>(resultItem.ToString());
+                        editorial.Editoriales.Add(resultItemList);
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Ocurrio un error al hacer la consulta";
+                    return View("Modal");
+                }
+
+            }
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5235/api/");
+
+                var responseTask = client.PostAsJsonAsync("Libro/GetAll", libro);
+                responseTask.Wait();
+
+                var resultUsuario = responseTask.Result;
+
+                if (resultUsuario.IsSuccessStatusCode)
+                {
+                    var readTask = resultUsuario.Content.ReadAsAsync<ML.Result>();
+                    readTask.Wait();
+
+                    foreach (var resultItem in readTask.Result.Objects)
+                    {
+                        ML.Libro resultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Libro>(resultItem.ToString());
+                        libro.Libros.Add(resultItemList);
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Ocurrio un error al hacer la consulta";
+                    return View("Modal");
+                }
+                libro.Autor = new ML.Autor();
+                libro.Autor.Autores = autor.Autores;
+                libro.Editorial = new ML.Editorial();
+                libro.Editorial.Editoriales = editorial.Editoriales;
+            }
+            return View(libro);
+        }
     }
 }
